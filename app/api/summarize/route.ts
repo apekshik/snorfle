a2832105b4c5ca78a1d98fd87cd3437e01b5c611
George Lydakis <george@lydakis.me>
2024-10-12 15:16:23 -0700

    Updated summarize endpoint

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -2,6 +2,28 @@ import { NextRequest, NextResponse } from 'next/server';
 import FirecrawlApp, { CrawlParams, CrawlStatusResponse } from '@mendable/firecrawl-js';
 import OpenAI from "openai";
 
+
+interface PostInput {
+  prompt: string;
+  displayLink: string;
+  link: URL;
+  title: string;
+  snippet: string;
+}
+
+interface ScrapeResponse {
+  displayLink: string;
+  link: URL;
+  title: string;
+  snippet: string;
+  markdown?: string;
+}
+
+interface PostResponse {
+  prompt: string;
+  result: ScrapeResponse;
+}
+
 export async function GET() {
   const apiKey = process.env.FIRECRAWL_API_KEY;
   const firecrawlApp = new FirecrawlApp({ apiKey: apiKey });
@@ -23,6 +45,36 @@ export async function GET() {
 export async function POST(request: NextRequest) {
   const apiKey = process.env.FIRECRAWL_API_KEY;
   const firecrawlApp = new FirecrawlApp({ apiKey: apiKey });
+  const xAi = new OpenAI();
 
-  return NextResponse.json({ message: 'OK' }, { status: 200 });
+  try {
+    // Parse the JSON body from the request
+    const body: PostInput = await request.json();
+
+    console.log(body);
+
+    const scrapeResponse = await firecrawlApp.scrapeUrl(body.link.toString(), {
+      formats: ['markdown'],
+    });
+
+    if (!scrapeResponse.success) {
+      throw new Error(`Failed to scrape: ${scrapeResponse.error}`)
+    }
+
+    const response: PostResponse = {
+      prompt: body.prompt,
+      result: {
+        displayLink: body.displayLink,
+        link: body.link,
+        title: body.title,
+        snippet: body.snippet,
+        markdown: scrapeResponse.markdown,
+      },
+    }
+
+    return NextResponse.json({ response }, { status: 200 });
+  } catch (error) {
+    console.error('Error in POST /api/summarize:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
 }
