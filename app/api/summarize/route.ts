3f59df2effa8929fa7a5e02be449bd02dea2d393
George Lydakis <george@lydakis.me>
2024-10-12 14:21:24 -0700

    Use firecrall and import open ai

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -1,5 +1,19 @@
 import { NextResponse } from 'next/server';
+import FirecrawlApp, { CrawlParams, CrawlStatusResponse } from '@mendable/firecrawl-js';
 
 export async function GET() {
-  return NextResponse.json({ message: 'OK' }, { status: 200 });
+  const apiKey = process.env.FIRECRAWL_API_KEY;
+  const firecrawlApp = new FirecrawlApp({ apiKey: apiKey });
+
+  const scrapeResponse = await firecrawlApp.scrapeUrl('https://sf.eater.com/maps/best-seafood-restaurants-san-francisco', {
+    formats: ['markdown', 'html'],
+  });
+
+  console.log(scrapeResponse);
+
+  if (!scrapeResponse.success) {
+    throw new Error(`Failed to scrape: ${scrapeResponse.error}`)
+  }
+
+  return NextResponse.json({ message: 'OK', response: scrapeResponse }, { status: 200 });
 }
