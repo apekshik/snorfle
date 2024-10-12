9062f6d3ae6f33fc5dde9c0d0c0ed4f509ea4ce9
George Lydakis <george@lydakis.me>
2024-10-12 14:41:34 -0700

    Added post endpoint

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import FirecrawlApp, { CrawlParams, CrawlStatusResponse } from '@mendable/firecrawl-js';
+import OpenAI from "openai";
 
 export async function GET() {
   const apiKey = process.env.FIRECRAWL_API_KEY;
   const firecrawlApp = new FirecrawlApp({ apiKey: apiKey });
+  const xAi = new OpenAI();
 
   const scrapeResponse = await firecrawlApp.scrapeUrl('https://sf.eater.com/maps/best-seafood-restaurants-san-francisco', {
     formats: ['markdown', 'html'],
@@ -16,4 +18,11 @@ export async function GET() {
   }
 
   return NextResponse.json({ message: 'OK', response: scrapeResponse }, { status: 200 });
+}
+
+export async function POST(request: NextRequest) {
+  const apiKey = process.env.FIRECRAWL_API_KEY;
+  const firecrawlApp = new FirecrawlApp({ apiKey: apiKey });
+
+  return NextResponse.json({ message: 'OK' }, { status: 200 });
 }
